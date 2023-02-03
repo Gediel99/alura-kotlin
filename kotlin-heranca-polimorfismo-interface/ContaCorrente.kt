dbb05f54e6944d240289edad9bbf16e625804da4
Gediel Durães de Almeida <95309923+Gediel99@users.noreply.github.com>
2023-02-03 17:34:25 -0300

    Adiciona a classe Conta Corrente

---
+++ kotlin-heranca-polimorfismo-interface/ContaCorrente.kt
@@ -0,0 +1,14 @@
+package `kotlin-heranca-polimorfismo-interface`
+
+class ContaCorrente(
+    titular: String,
+    id: Int,
+) : Conta(
+    titular = id,
+    id = titular
+){
+    override fun sacar(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        super.sacar(valor)
+    }
+}

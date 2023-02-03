68f884b6469ba34ba590fea16394fb01ae33f03d
Gediel Durães de Almeida <95309923+Gediel99@users.noreply.github.com>
2023-02-03 17:53:25 -0300

    Atualiza as informações do arquivo main

--- kotlin-heranca-polimorfismo-interface/main.kt
+++ kotlin-heranca-polimorfismo-interface/main.kt
@@ -3,6 +3,29 @@ package `kotlin-heranca-polimorfismo-interface`
 fun main() {
     println("---------- Bem vindo ao Bytebank ----------")
 
-    testaFuncionarios()
+    val contaCorrente = ContaCorrente(
+        titular = "Alex",
+        id = 1000
+    )
+
+    val contaPoupanca = ContaPoupanca(
+        titular = "Fran",
+        id = 1001
+    )
+
+    contaPoupanca.depositar(1000.0)
+    contaCorrente.depositar(1000.0)
+
+    contaPoupanca.sacar(100.0)
+    contaCorrente.sacar(100.0)
+
+    println("Saldo após saque corrente: ${contaCorrente.saldo}")
+    println("Saldo após saque poupança: ${contaPoupanca.saldo}")
+
+    contaCorrente.transferir(100.0, contaPoupanca)
+
+    println("Saldo após transferir para poupança: ${contaCorrente.saldo}")
+    println("Saldo após receber tranferência: ${contaPoupanca.saldo}")
+
 }
 
